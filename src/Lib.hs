416da0679f77bc928a1d0f4b15e701e5d944582a
Sandy Maguire <sandy@sandymaguire.me>
2019-08-19 14:00:30 -0600

    ¯\_(ツ)_/¯

--- src/Lib.hs
+++ src/Lib.hs
@@ -1,70 +1,106 @@
-{-# LANGUAGE TupleSections #-}
+{-# LANGUAGE BlockArguments #-}
+{-# LANGUAGE LambdaCase     #-}
+{-# LANGUAGE TupleSections  #-}
 
 module Lib where
 
+import Data.List hiding (uncons)
+import Control.Monad
+import Data.Foldable
 import Control.Arrow hiding (first, second)
 import Data.Bifunctor
 import Data.Char
-import Control.Lens
+import Control.Lens hiding (cons)
 import Data.Tuple
 
-type Parser s a = Prism' s (a, s)
+type Parser s a = Iso' (Maybe s) (Maybe (a, s))
 
 
 ffmap :: (Functor f, Bifunctor p) => (a -> b) -> f (p a c) -> f (p b c)
 ffmap f = fmap (first f)
 
-space :: Parser String ()
-space = prism' ((' ' :) . snd)
-               ( ffmap (const ())
-               . preview (many $ satisfy isSpace)
-               )
+-- space :: Parser String ()
+-- space = prism' ((' ' :) . snd)
+--                ( ffmap (const ())
+--                . preview (many $ satisfy isSpace)
+--                )
+
+cons :: (a, [a]) -> [a]
+cons = uncurry (:)
+
+isoM :: Iso' a b -> Iso' (Maybe a) (Maybe b)
+isoM = mapping
 
 anyChar :: Parser String Char
-anyChar = prism' (uncurry (:)) (uncons)
+anyChar = iso (>>= uncons) (fmap cons)
+
+isoP
+    :: (Monad m)
+    => (a -> m b)
+    -> (b -> m a)
+    -> Iso' (m a) (m b)
+isoP f g = iso (>>= f) (>>= g)
 
 satisfy :: (a -> Bool) -> Parser [a] a
-satisfy f = prism' (uncurry (:)) $ \zs ->
-  case uncons zs of
-    z@(Just (a, as)) | f a -> z
-    _                      -> Nothing
+satisfy f = isoP fuck you_chris
+  where
+    fuck (a : as) = do
+      guard $ f a
+      pure (a, as)
 
-char :: Char -> Parser String Char
+    you_chris (a, as) = do
+      guard $ f a
+      pure (a : as)
+
+char :: Eq a => a -> Parser [a] a
 char c = satisfy (== c)
 
-many :: Parser s a -> Parser s [a]
-many p =
-  prism'
-    (\(la, s) -> foldr (curry $ review p) s la)
-    (\s ->
-      case preview p s of
-        Just (a, s') -> do
-          (as, s'') <- preview (many p) s'
-          pure $ (a : as, s'')
-        Nothing -> pure ([], s)
-      )
+many :: Monoid s => Parser s a -> Parser s [a]
+many p = isoP fuck you_chris
+  where
+    fuck s = do
+      (a, s')   <- view p $ Just s
+      (as, s'') <- view (many p) $ Just s'
+      pure ((a : as), s'')
 
-pprint :: Monoid s => Parser s a -> a -> s
-pprint p = review p . (, mempty)
+    you_chris (as, s) =
+      fmap ((<> s) . fold)
+        $ traverse (review p . Just . (, mempty)) as
 
-tupleP :: Monoid s => Parser s a -> Parser s b -> Parser s (a, b)
-tupleP pa pb =
-  prism' (\((a, b), s) ->
-    let s' = review pb (b, s)
-     in review pa (a, s')
-    ) $ \s -> do
-  (a, s')  <- preview pa s
-  (b, s'') <- preview pb s'
-  pure ((a, b), s'')
+-- tupleP :: Monoid s => Parser s a -> Parser s b -> Parser s (a, b)
+-- tupleP pa pb =
+--   prism' (\((a, b), s) ->
+--     let s' = review pb (b, s)
+--      in review pa (a, s')
+--     ) $ \s -> do
+--   (a, s')  <- preview pa s
+--   (b, s'') <- preview pb s'
+--   pure ((a, b), s'')
 
 
-firstP :: Prism' s (a, b) -> Iso' a a' -> Prism' s (a', b)
-firstP p i = prism' (\z -> review p $ z & _1 %~ view (from i)) $ \s -> do
-  z <- preview p s
-  pure $ z & _1 %~ view i
+-- firstP :: Prism' s (a, b) -> Iso' a a' -> Prism' s (a', b)
+-- firstP p i = prism' (\z -> review p $ z & _1 %~ view (from i)) $ \s -> do
+--   z <- preview p s
+--   pure $ z & _1 %~ view i
 
 
-string :: String -> Parser String String
-string "" = iso ("",) fst
-string (a : as) = firstP (tupleP (char a) (string as)) $ iso (uncurry (:)) (head &&& tail)
+string :: Eq a => [a] -> Parser [a] [a]
+string str = isoP (fmap (str, ) . stripPrefix str)
+  \(str', lo) -> do
+      guard $ str == str'
+      pure $ str' <> lo
 
+-- apP :: Parser s (AnIso' a b) -> Parser s a -> Parser s b
+-- apP pf pa =
+--   isoP (\s -> do
+--     (f, s')  <- view pf $ Just s
+--     (a, s'') <- view pa $ Just s'
+--     pure (view (cloneIso f) a, s'')
+--     )
+--   _
+
+appP
+    :: Iso' s (s', AnIso' a b)
+    -> Iso' s' (s'', a)
+    -> Iso' s (s'', b)
+appP pf pa = iso undefined $ \(s'', b) -> _
